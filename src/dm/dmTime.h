86046b0e18a5ad3096c25e32ae502de772cbe5e2
Patrick Wensing <pwensing@gmail.com>
2012-01-03 13:23:16 -0500

    Added extra switch to dmTime.h fix Mac problem

--- src/dm/dmTime.h
+++ src/dm/dmTime.h
@@ -59,7 +59,7 @@ inline void dmGetSysTime(dmTimespec *ts)
     ts->tv_sec = timebuffer.time;
     ts->tv_nsec = timebuffer.millitm*1000000;
 
-#elif defined(_POSIX_TIMERS)
+#elif defined(_POSIX_TIMERS) && !defined(__APPLE__)
     if (clock_gettime(CLOCK_REALTIME, ts) != 0)
     {
         throw ts;

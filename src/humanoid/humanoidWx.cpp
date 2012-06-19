b58ccc6bfd6635b6b9d17b1e336aefea983ef816
Yiping <engineerlyp@gmail.com>
2012-06-19 16:39:18 -0400

    CMake project for humanoid; ubuntu compatibility update for humanoid

--- src/humanoid/humanoidWx.cpp
+++ src/humanoid/humanoidWx.cpp
@@ -3,6 +3,8 @@
 #include "wx/glcanvas.h"
 #include "BasicGLPane.h"
 
+//This is a dirty workaround
+#undef Success
 
 #include <dm.h>            // DynaMechs typedefs, globals, etc.
 #include <dmu.h>
@@ -304,6 +306,9 @@ BasicGLPane::BasicGLPane(wxFrame* parent, int* args, const wxSize &size) :
     // To avoid flashing on MSW
     SetBackgroundStyle(wxBG_STYLE_CUSTOM);
 
+    int argc = 1;
+    char* argv[1] = { wxString((wxTheApp->argv)[0]).char_str()};
+	glutInit(&argc, argv);
 }
   
 void BasicGLPane::resized(wxSizeEvent& evt) {
@@ -553,12 +558,13 @@ void myInit (void) {
 
 	// ****
 	glEnable(GL_LINE_SMOOTH);
-	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
 	// Enable Blending
 	glEnable(GL_BLEND);
 	// Specifies pixel arithmetic
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
+	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glLineWidth (1.5);
 	// ****
 	//glShadeModel(GL_FLAT);
 	glShadeModel(GL_SMOOTH);

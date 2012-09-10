60dfe1919653e4e4ff226fa2fab7f8987ac533f7
Yiping <engineerlyp@gmail.com>
2012-09-10 13:45:57 -0400

    humanoid controller switch back using TaskSpaceController A (one layer using weighted QP)

--- src/humanoid/h/HumanoidController.h
+++ src/humanoid/h/HumanoidController.h
@@ -16,7 +16,7 @@
 
 #define COPY_P_TO_VEC(p,pvec) pvec << p[0], p[1], p[2]; 
 
-class HumanoidController : public Humanoid , public TaskSpaceControllerB {
+class HumanoidController : public Humanoid , public TaskSpaceControllerA {
 public:
 	HumanoidController(dmArticulation * robot);
 	void HumanoidControl(ControlInfo &);
